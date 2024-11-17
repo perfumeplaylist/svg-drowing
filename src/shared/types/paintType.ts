9d264b9d72894a7e50c96180b091587a2def9c15
kim <perfumeplaylist@gmail.com>
2024-11-17 18:06:14 +0900

    feat: paint에 관련 type 정의

---
+++ src/shared/types/paintType.ts
@@ -0,0 +1,51 @@
+import paintType from '../constant/paintInfo';
+
+export interface DrawingInfo {
+  mode: {
+    tool: keyof typeof paintType.PAINT_TYPE | '';
+    state: keyof typeof paintType.PAINT_FN_TYPE | '';
+  };
+  thickness: number;
+  colorInfo: typeof paintType.COLOR_TYPE;
+}
+
+export interface DrawingInfoProps {
+  strokeWidth: number;
+  strokeColor: string;
+  color?: string;
+  type: string;
+  key: number;
+}
+
+export interface LineListType extends DrawingInfoProps {
+  points: number[];
+}
+
+export interface CircleListType extends DrawingInfoProps {
+  x: number;
+  y: number;
+  rx: number;
+  ry: number;
+}
+
+export interface RectListType extends DrawingInfoProps {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+}
+
+export interface PolygonListType extends LineListType {
+  isComplete: boolean;
+}
+
+export interface CurveListType extends DrawingInfoProps {
+  x1: number;
+  y1: number;
+  x2: number;
+  y2: number;
+  controlX: number;
+  controlY: number;
+  state: string;
+  d: string;
+}
