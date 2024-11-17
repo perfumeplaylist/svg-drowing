b8ca35b2e36fce1d5de37642fdd0852a01fabde7
kim <perfumeplaylist@gmail.com>
2024-11-17 18:04:37 +0900

    fix: drawing 데이터 구조 수정 및 ui 수정

---
+++ src/features/paintCanvas/ui.tsx
@@ -0,0 +1,421 @@
+import type { StageProps } from 'react-konva';
+import { useDrawingContext } from './context/useDrawing';
+import { useEffect, useRef, useState } from 'react';
+import useLocalStorage from '../../shared/hooks/useLocalStorage';
+import type {
+  CircleListType,
+  CurveListType,
+  LineListType,
+  PolygonListType,
+  RectListType,
+  DrawingInfoProps,
+} from '../../shared/types/paintType';
+import Canvas from '../../shared/components/Paint';
+import paintType from '../../shared/constant/paintType';
+
+interface TotalPaintInfoType {
+  data: (
+    | LineListType
+    | CircleListType
+    | RectListType
+    | PolygonListType
+    | CurveListType
+  )[];
+}
+
+const DrawingCanvas = () => {
+  const {
+    drawingInfo: { mode, thickness, colorInfo },
+    handleChangeModeState,
+  } = useDrawingContext();
+
+  const [storage, setValue] = useLocalStorage<TotalPaintInfoType>(
+    paintType.LOCALSTORAGE_KEY,
+    {
+      data: [],
+    }
+  );
+
+  const [totalPaintInfo, setTotalPaintInfo] =
+    useState<TotalPaintInfoType>(storage);
+
+  const isDrawing = useRef<boolean>(false);
+  const isCurving = useRef<boolean>(false);
+  const undoArr = useRef<TotalPaintInfoType['data']>([]);
+  const redoArr = useRef<TotalPaintInfoType['data']>([]);
+
+  // mousedown이 되었을때 어떻게 동작되는지 알아야할까?
+  // 어떤게 업데이트가 되어서 화면에 표시되는지가 더 중요하지 않을까?
+  const handleMouseDown = ({ target }: StageProps) => {
+    if (!mode.tool.length) return;
+    const { x, y } = target.getStage().getPointerPosition();
+    isDrawing.current = true;
+
+    const newShape = {
+      type: mode.tool,
+      key: Date.now(),
+      strokeWidth: thickness,
+      strokeColor: colorInfo.stoke,
+      color: colorInfo.fill,
+    } as DrawingInfoProps;
+
+    switch (mode.tool) {
+      case 'line': {
+        setTotalPaintInfo((prev) => ({
+          ...prev,
+          data: [
+            ...prev.data,
+            {
+              ...newShape,
+              points: [x, y, x, y],
+            } as LineListType,
+          ],
+        }));
+        break;
+      }
+      case 'circle': {
+        setTotalPaintInfo((prev) => ({
+          ...prev,
+          data: [
+            ...prev.data,
+            {
+              ...newShape,
+              x,
+              y,
+              rx: 0,
+              ry: 0,
+            } as CircleListType,
+          ],
+        }));
+        break;
+      }
+      case 'rect': {
+        setTotalPaintInfo((prev) => ({
+          ...prev,
+          data: [
+            ...prev.data,
+            {
+              ...newShape,
+              x,
+              y,
+              width: 0,
+              height: 0,
+            } as RectListType,
+          ],
+        }));
+        break;
+      }
+      case 'curve': {
+        if (!isCurving.current) {
+          const newTempLine = {
+            x1: x,
+            y1: y,
+            x2: x,
+            y2: y,
+            state: 'dashLine',
+          };
+          setTotalPaintInfo((prev) => ({
+            ...prev,
+            data: [
+              ...prev.data,
+              {
+                ...newShape,
+                ...newTempLine,
+              } as CurveListType,
+            ],
+          }));
+          break;
+        }
+        const lastCurveIndex = totalPaintInfo.data
+          .slice()
+          .reverse()
+          .findIndex((shape) => shape.type === 'curve');
+        if (lastCurveIndex !== -1) {
+          const actualIndex = totalPaintInfo.data.length - 1 - lastCurveIndex;
+          const lastCurve = totalPaintInfo.data[actualIndex] as CurveListType;
+
+          const pathData = `M ${lastCurve.x1} ${lastCurve.y1} Q ${lastCurve.controlX} ${lastCurve.controlY} ${lastCurve.x2} ${lastCurve.y2}`;
+
+          const updatedCurve = {
+            ...lastCurve,
+            state: 'curve',
+            d: pathData,
+          };
+
+          setTotalPaintInfo((prev) => ({
+            ...prev,
+            data: prev.data.map((shape, index) =>
+              index === actualIndex ? updatedCurve : shape
+            ),
+          }));
+        }
+        isCurving.current = false;
+        isDrawing.current = false;
+        break;
+      }
+      case 'polygon': {
+        const lastPolygonIndex = totalPaintInfo.data.findIndex(
+          (shape) =>
+            shape.type === 'polygon' && !(shape as PolygonListType).isComplete
+        );
+
+        if (lastPolygonIndex === -1) {
+          setTotalPaintInfo((prev) => ({
+            ...prev,
+            data: [
+              ...prev.data,
+              {
+                ...newShape,
+                points: [x, y, x, y],
+                isComplete: false,
+              } as PolygonListType,
+            ],
+          }));
+          break;
+        }
+
+        const updatedData = totalPaintInfo.data.map((shape, index) => {
+          if (lastPolygonIndex === index)
+            return {
+              ...(shape as PolygonListType),
+              points: [...(shape as PolygonListType).points, x, y],
+            };
+          return shape;
+        });
+
+        setTotalPaintInfo((prev) => ({
+          ...prev,
+          data: updatedData,
+        }));
+        break;
+      }
+    }
+  };
+
+  const handleMouseMove = ({ target }: StageProps) => {
+    if (!isDrawing.current || !mode.tool.length) return;
+    const { x, y } = target.getStage().getPointerPosition();
+
+    const lastIndex = totalPaintInfo.data.length - 1;
+    const lastShape = totalPaintInfo.data[lastIndex];
+
+    switch (mode.tool) {
+      case 'line': {
+        const updatedLine = {
+          ...lastShape,
+          points: [
+            (lastShape as LineListType).points[0],
+            (lastShape as LineListType).points[1],
+            x,
+            y,
+          ],
+        } as LineListType;
+
+        setTotalPaintInfo((prev) => ({
+          ...prev,
+          data: [...prev.data.slice(0, -1), updatedLine],
+        }));
+        break;
+      }
+      case 'circle': {
+        const updatedCircle = {
+          ...lastShape,
+          rx: Math.abs((lastShape as CircleListType).x - x),
+          ry: Math.abs((lastShape as CircleListType).y - y),
+        } as CircleListType;
+
+        setTotalPaintInfo((prev) => ({
+          ...prev,
+          data: [...prev.data.slice(0, -1), updatedCircle],
+        }));
+        break;
+      }
+      case 'rect': {
+        const updatedRect = {
+          ...lastShape,
+          width: x - (lastShape as RectListType).x,
+          height: y - (lastShape as RectListType).y,
+        } as RectListType;
+
+        setTotalPaintInfo((prev) => ({
+          ...prev,
+          data: [...prev.data.slice(0, -1), updatedRect],
+        }));
+        break;
+      }
+      case 'curve': {
+        if (!isCurving.current) {
+          const updateCurve = {
+            ...lastShape,
+            x2: x,
+            y2: y,
+          } as CurveListType;
+
+          setTotalPaintInfo((prev) => ({
+            data: [...prev.data.slice(0, -1), updateCurve],
+          }));
+
+          break;
+        }
+        const updateCurve = {
+          ...lastShape,
+          controlX: x,
+          controlY: y,
+        };
+
+        setTotalPaintInfo((prev) => ({
+          ...prev,
+          data: [...prev.data.slice(0, -1), updateCurve],
+        }));
+        break;
+      }
+      case 'polygon': {
+        const lastPolygonIndex = totalPaintInfo.data.findIndex(
+          (shape) =>
+            shape.type === 'polygon' && !(shape as PolygonListType).isComplete
+        );
+
+        if (lastPolygonIndex !== -1) {
+          const updatedData = totalPaintInfo.data.map((shape, index) =>
+            index === lastPolygonIndex
+              ? {
+                  ...(shape as PolygonListType),
+                  points: [
+                    ...(shape as PolygonListType).points.slice(0, -2),
+                    x,
+                    y,
+                  ],
+                }
+              : shape
+          );
+          setTotalPaintInfo((prev) => ({
+            ...prev,
+            data: updatedData,
+          }));
+        }
+        break;
+      }
+    }
+  };
+
+  const handleMouseUp = ({ target }: StageProps) => {
+    if (mode.tool === 'curve') {
+      const lastCurveIndex = totalPaintInfo.data.findIndex(
+        (shape) =>
+          shape.type === 'curve' &&
+          (shape as CurveListType).state === 'dashLine'
+      );
+
+      if (lastCurveIndex !== -1) {
+        const lastCurve = totalPaintInfo.data[lastCurveIndex] as CurveListType;
+        const updatedCurve = {
+          ...lastCurve,
+          state: 'dashCurve',
+          controlX: lastCurve.x2,
+          controlY: lastCurve.y2,
+        };
+
+        setTotalPaintInfo((prev) => ({
+          ...prev,
+          data: prev.data.map((shape, index) =>
+            index === lastCurveIndex ? updatedCurve : shape
+          ),
+        }));
+        isCurving.current = true;
+      }
+    }
+
+    if (mode.tool === 'polygon') {
+      const { x, y } = target.getStage().getPointerPosition();
+      const lastPolygonIndex = totalPaintInfo.data.findIndex(
+        (shape) =>
+          shape.type === 'polygon' && !(shape as PolygonListType).isComplete
+      );
+
+      if (lastPolygonIndex !== -1) {
+        const lastPolygon = totalPaintInfo.data[
+          lastPolygonIndex
+        ] as PolygonListType;
+        const startX = lastPolygon.points[0];
+        const startY = lastPolygon.points[1];
+
+        const isNearStart =
+          Math.abs(x - startX) < 10 && Math.abs(y - startY) < 10;
+        const updatedData = totalPaintInfo.data.map((shape, index) =>
+          index === lastPolygonIndex
+            ? {
+                ...lastPolygon,
+                points: isNearStart
+                  ? [...lastPolygon.points.slice(0, -2), startX, startY]
+                  : [...lastPolygon.points, x, y],
+                isComplete: isNearStart,
+              }
+            : shape
+        );
+
+        setTotalPaintInfo((prev) => ({
+          ...prev,
+          data: updatedData,
+        }));
+
+        if (isNearStart) {
+          undoArr.current.push(
+            totalPaintInfo.data[totalPaintInfo.data.length - 1]
+          );
+          if (undoArr.current.length > 40) undoArr.current.shift();
+        }
+      }
+      isDrawing.current = false;
+    }
+  };
+
+  useEffect(() => {
+    if (redoArr.current.length > 40) {
+      redoArr.current.shift();
+    }
+    setValue(totalPaintInfo);
+  }, [totalPaintInfo, setValue]);
+
+  useEffect(() => {
+    if (mode.state === 'undo') {
+      if (totalPaintInfo.data.length > 0) {
+        const lastPaint = totalPaintInfo.data[totalPaintInfo.data.length - 1];
+        undoArr.current.push(lastPaint);
+        const newPaintData = totalPaintInfo.data.slice(0, -1);
+
+        setTotalPaintInfo((prev) => ({
+          ...prev,
+          data: newPaintData,
+        }));
+
+        handleChangeModeState('');
+      }
+    } else if (mode.state === 'redo') {
+      if (undoArr.current.length > 0) {
+        const redoPaint = undoArr.current.pop()!;
+        setTotalPaintInfo((prev) => ({
+          ...prev,
+          data: [...prev.data, redoPaint],
+        }));
+
+        handleChangeModeState('');
+      }
+    }
+  }, [mode.state, totalPaintInfo]);
+
+  return (
+    <Canvas.Stage
+      onMouseDown={handleMouseDown}
+      onMouseMove={handleMouseMove}
+      onMouseUp={handleMouseUp}
+    >
+      <Canvas.Layer>
+        {totalPaintInfo.data.map((paint) => (
+          <Canvas.Paint {...paint} key={paint.key} />
+        ))}
+      </Canvas.Layer>
+    </Canvas.Stage>
+  );
+};
+
+export default DrawingCanvas;
---
+++ src/shared/components/Paint/index.tsx
@@ -0,0 +1,154 @@
+import {
+  Ellipse as KonvaEllipse,
+  Line as KonvaLine,
+  Rect as KonvaRect,
+  Path as KonvaPath,
+  Layer as KonvaLayer,
+  Stage as KonvaStage,
+  StageProps as KonvaStageProps,
+} from 'react-konva';
+
+import type {
+  LineListType,
+  CircleListType,
+  RectListType,
+  PolygonListType,
+  CurveListType,
+} from '../../types/paintType';
+
+import { PropsWithChildren } from 'react';
+
+type PaintProps =
+  | LineListType
+  | CircleListType
+  | RectListType
+  | PolygonListType
+  | CurveListType;
+
+const Stage = ({ children, ...props }: PropsWithChildren<KonvaStageProps>) => {
+  return (
+    <KonvaStage
+      width={window.innerWidth}
+      height={window.innerHeight}
+      {...props}
+    >
+      {children}
+    </KonvaStage>
+  );
+};
+
+const Layer = ({ children }: PropsWithChildren) => {
+  return <KonvaLayer>{children}</KonvaLayer>;
+};
+
+// TODO:리펙토링 부분
+const Paint = ({ type, ...props }: PaintProps) => {
+  if (type === 'line') {
+    const lineProps = props as LineListType;
+    return (
+      <KonvaLine
+        {...lineProps}
+        points={lineProps.points}
+        stroke={lineProps.strokeColor}
+        strokeWidth={lineProps.strokeWidth}
+      />
+    );
+  }
+
+  if (type === 'circle') {
+    const circleProps = props as CircleListType;
+    return (
+      <KonvaEllipse
+        {...circleProps}
+        x={circleProps.x}
+        y={circleProps.y}
+        radiusX={circleProps.rx}
+        radiusY={circleProps.ry}
+        fill={circleProps.color}
+        stroke={circleProps.strokeColor}
+        strokeWidth={circleProps.strokeWidth}
+      />
+    );
+  }
+
+  if (type === 'rect') {
+    const rectProps = props as RectListType;
+    return (
+      <KonvaRect
+        {...rectProps}
+        x={rectProps.x}
+        y={rectProps.y}
+        width={rectProps.width}
+        height={rectProps.height}
+        fill={rectProps.color}
+        stroke={rectProps.strokeColor}
+        strokeWidth={rectProps.strokeWidth}
+      />
+    );
+  }
+
+  if (type === 'curve') {
+    const curveProps = props as CurveListType;
+    switch (curveProps.state) {
+      case 'dashLine': {
+        return (
+          <KonvaLine
+            points={[
+              curveProps.x1,
+              curveProps.y1,
+              curveProps.x2,
+              curveProps.y2,
+            ]}
+            stroke={curveProps.strokeColor}
+            strokeWidth={curveProps.strokeWidth}
+            dash={[10, 5]}
+          />
+        );
+      }
+      case 'dashCurve': {
+        return (
+          <KonvaPath
+            data={`M ${curveProps.x1} ${curveProps.y1} Q ${curveProps.controlX} ${curveProps.controlY} ${curveProps.x2} ${curveProps.y2}`}
+            stroke={curveProps.strokeColor}
+            strokeWidth={curveProps.strokeWidth}
+            dash={[5, 5]}
+          />
+        );
+      }
+      case 'curve': {
+        return (
+          <KonvaPath
+            key={curveProps.key}
+            data={curveProps.d}
+            stroke={curveProps.strokeColor}
+            strokeWidth={curveProps.strokeWidth}
+          />
+        );
+      }
+    }
+  }
+
+  if (type === 'polygon') {
+    const polygonProps = props as PolygonListType;
+    return (
+      <KonvaLine
+        {...polygonProps}
+        points={polygonProps.points}
+        stroke={polygonProps.strokeColor}
+        strokeWidth={polygonProps.strokeWidth}
+        closed={polygonProps.isComplete}
+        fill={polygonProps.isComplete ? polygonProps.color : 'none'}
+      />
+    );
+  }
+
+  return null;
+};
+
+const Canvas = {
+  Stage,
+  Layer,
+  Paint,
+};
+
+export default Canvas;
