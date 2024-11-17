7c50d81745186c25bb7435e6c1cbbf0162fcfc6a
kim <perfumeplaylist@gmail.com>
2024-11-17 18:09:57 +0900

    fix: 폴더구조에 맞게 파일 수정

--- src/layout/Paint/Canvas.tsx
+++
@@ -1,9 +0,0 @@
-import { PropsWithChildren } from 'react';
-
-import * as S from './Paint.style';
-
-const Canvas = ({ children }: PropsWithChildren) => {
-  return <S.Canvas>{children}</S.Canvas>;
-};
-
-export default Canvas;
